9d36ce09cb783357d628f689ff52c258a98a375e
chriskim2311 <chriskim2311@gmail.com>
2018-11-16 15:47:21 -0800

    completed setting up redux and routing

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import {combineReducers} from 'redux';
+import listReducer from './list_reducer';
+
+const rootReducer = combineReducers({
+    list: listReducer
+});
+
+
+
+export default rootReducer;
+
+
+
+
