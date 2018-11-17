4b966bedb7b37ffa164e39b6e450f8a899396c70
chriskim2311 <chriskim2311@gmail.com>
2018-11-16 18:02:37 -0800

    added redux form

--- src/components/app.js
+++ src/components/app.js
@@ -3,11 +3,14 @@ import 'materialize-css/dist/css/materialize.min.css'
 import 'materialize-css/dist/js/materialize';
 import {Route} from 'react-router-dom';
 import List from './list';
+import AddItem from './add_item';
+
 
 
 const App = () => (
     <div className="container">
     <Route path="/" exact component={List}/>
+    <Route path='/add-item' component={AddItem}/>
 
 
     </div>
