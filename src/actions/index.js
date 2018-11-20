1c9c566960f9548f07867136929a1ce740941202
chriskim2311 <chriskim2311@gmail.com>
2018-11-20 15:20:10 -0800

    Completed add item to server functionality

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,16 @@ import axios from 'axios';
 const BASE_URL = 'http://api.reactprototypes.com/todos';
 const API_KEY = '?key=c918_chriskim';
 
+export function addToDoItem(item) {
+    const resp= axios.post(BASE_URL +API_KEY, item);
+
+    return {
+        type: types.ADD_LIST_ITEM,
+        payload: resp
+    }
+
+}
+
 export function getListData(){
     const resp = axios.get(BASE_URL +API_KEY);
 
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,5 @@
 export default {
+    ADD_LIST_ITEM: 'add_list_item',
     GET_ALL_LIST_DATA: 'get_all_list_data'
 }
 
